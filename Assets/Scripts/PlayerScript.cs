5bb3b3f404b5291275d3890fbcccfa96f5ce964c
Tyrell Whitby <tyrellwhitby@gmail.com>
2020-10-25 13:23:59 -0400

    challenge commit

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Globalization;
 using System.Runtime.CompilerServices;
 using UnityEngine;
@@ -12,31 +13,65 @@ public class PlayerScript : MonoBehaviour
     public float speed;
     public Text score;
     private int scoreValue = 0;
+    public Text winText;
+    public Text livesText;
+    public int livesValue = 3;
+    public AudioSource musicSource;
+    public AudioClip musicClipOne;
+    public AudioClip musicClipTwo;
 
     // Start is called before the first frame update
     void Start()
     {
         rd2d = GetComponent<Rigidbody2D>();
         score.text = scoreValue.ToString();
+        winText.text = "";
+        livesText.text = livesValue.ToString();
+        musicSource.clip = musicClipOne;
+        musicSource.Play();
+        musicSource.loop = true;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float hozMovement = Input.GetAxis("Horizontal");
         float verMovement = Input.GetAxis("Vertical");
 
         rd2d.AddForce(new Vector2(hozMovement * speed, verMovement * speed));
-
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) 
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-    if(collision.collider.tag == "Coin") 
+        if (collision.collider.tag == "Coin")
         {
             scoreValue += 1;
             score.text = scoreValue.ToString();
             Destroy(collision.collider.gameObject);
+            if (scoreValue == 4)
+            {
+                transform.position = new Vector2(60.0f, 4.0f);
+                livesValue = 3;
+                livesText.text = livesValue.ToString();
+            }
+            if (scoreValue == 8)
+            {
+                winText.text = "You win! -Tyrell Whitby";
+                musicSource.clip = musicClipTwo;
+                musicSource.Play();
+            }
+        }
+        if (collision.collider.tag == "Enemy")
+        {
+            livesValue -= 1;
+            livesText.text = livesValue.ToString();
+            Destroy(collision.collider.gameObject);
+            if (livesValue == 0)
+            {
+                winText.text = "You lose!";
+                Destroy(gameObject);
+            }
         }
     }
 
